42dcc6b03e7728b5812a27bed50b477942bef160
Umagowda1232 <umavgowda22@gmail.com>
2025-04-27 09:32:30 +0530

    Added menubar and sidebar

---
+++ script.js
@@ -0,0 +1,19 @@
+const toggleButton = document.querySelector('.menu-btn');
+const backDrop = document.querySelector('.backdrop');
+const mainNav = document.querySelector('.side-bar');
+
+toggleButton.addEventListener('click', () => {
+  if (mainNav.style.display === 'block') {
+    mainNav.style.display = 'none';
+    backDrop.style.display = 'none';
+  }
+  else {
+    backDrop.style.display = 'block';
+    mainNav.style.display = 'block';
+  }
+})
+
+backDrop.addEventListener('click', () => {
+  backDrop.style.display = 'none';
+  mainNav.style.display = 'none';
+})
